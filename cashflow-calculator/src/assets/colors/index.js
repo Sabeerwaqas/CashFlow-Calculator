73bc95e9734370df2d33d9f8560200d236da9689
Sabeerwaqas <sabeerwaqas050@gmail.com>
2024-04-01 22:24:04 +0500

    [Add: uuid and build buttons]

--- cashflow-calculator/src/assets/colors/index.js
+++ cashflow-calculator/src/assets/colors/index.js
@@ -1,7 +1,11 @@
 export const projectColors = {
-    darkTheme:"#092230",
-    lightTheme:"#F8F7F3",
-    darkThemeTextColor:"#F8F7F3",
-    lightThemeTextColor:"#092230",
-
-}
+  darkTheme: "#092230",
+  lightTheme: "#F8F7F3",
+  darkThemeTextColor: "#F8F7F3",
+  lightThemeTextColor: "#092230",
+  cancelBtnBgColor: "#FF0000",
+  clearBtnBgColor: "#FFFF00",
+  enterBtnBgColor: "#008000",
+  lightThemeBtnColor: "#000000",
+  darkThemeBtnColor: "#FFFFFF",
+};
--- cashflow-calculator/src/pages/home/Home.js
+++ cashflow-calculator/src/pages/home/Home.js
@@ -3,6 +3,8 @@ import { Box, Button, Container, Grid } from "@mui/material";
 import { projectColors } from "../../assets/colors/index";
 import LightModeIcon from "@mui/icons-material/LightMode";
 import DarkModeIcon from "@mui/icons-material/DarkMode";
+import { atmBtn } from "./index";
+import { v4 as uuidv4 } from 'uuid';
 
 const Home = () => {
   const [themeState, setThemeState] = useState(false);
@@ -22,17 +24,68 @@ const Home = () => {
               sx={{
                 width: "100%",
                 height: "90vh",
-                backgroundColor: "lightgreen",
+                backgroundColor:
+                  themeState === false
+                    ? projectColors.lightTheme
+                    : projectColors.darkTheme,
+                boxShadow: "0 1px 10px 1px grey",
                 p: 2,
                 mt: 3.5,
                 borderRadius: "10px",
               }}
             >
               <Grid container justifyContent={"space-between"} height={"100px"}>
-                <Grid item xs={6} sx={{ backgroundColor: "blue" }}>
-                  wrw
+              {/* Left side */}
+                <Grid
+                  item
+                  xs={6}
+                  sx={{
+                    backgroundColor:
+                      themeState === false
+                        ? projectColors.lightTheme
+                        : "#000000",
+                    boxShadow:
+                      themeState === false
+                        ? "0 1px 10px 1px grey"
+                        : "0 1px 10px 1px #ffffff",
+                    color: themeState === false ? "#000000" : "#ffffff",
+                    borderRadius: "10px",
+                    padding: "1%",
+                  }}
+                >
+                  <Box
+                    component={"section"}
+                    sx={{
+                      border:
+                        themeState === false
+                          ? "2px solid #000000"
+                          : "2px solid #ffffff",
+                      height: "20vh",
+                      borderRadius: "10px",
+                      backgroundColor:
+                        themeState === false
+                          ? "#ffffff"
+                          : projectColors.darkTheme,
+                    }}
+                  ></Box>
                 </Grid>
-                <Grid item xs={4} sx={{ backgroundColor: "lightblue" }}>
+                {/* Right side */}
+                <Grid
+                  item
+                  xs={4}
+                  sx={{
+                    backgroundColor:
+                      themeState === false
+                        ? projectColors.lightTheme
+                        : "#000000",
+                    boxShadow:
+                      themeState === false
+                        ? "0 1px 10px 1px grey"
+                        : "0 1px 10px 1px #ffffff",
+                    color: themeState === false ? projectColors.lightThemeBtnColor : projectColors.darkThemeBtnColor ,
+                    borderRadius: "10px",
+                  }}
+                >
                   {/* Dark mode and light mode */}
                   <Box component={"div"}>
                     <Button
@@ -70,7 +123,7 @@ const Home = () => {
                               fontSize: "15px",
                               pl: 1,
                               pr: 0.2,
-                              textTransform:"capitalize"
+                              textTransform: "capitalize",
                             }}
                           >
                             Dark Mode
@@ -91,7 +144,7 @@ const Home = () => {
                               fontSize: "15px",
                               pl: 1,
                               pr: 0.2,
-                              textTransform:"capitalize"
+                              textTransform: "capitalize",
                             }}
                           >
                             Light Mode
@@ -99,6 +152,87 @@ const Home = () => {
                         </Box>
                       )}
                     </Button>
+                  </Box>
+                  {/* ATM buttons */}
+                  <Box
+                    component={"div"}
+                    sx={{
+                      width: "90%",
+                      height: "auto",
+                      backgroundColor:
+                        themeState === false
+                          ? "#ffffff"
+                          : projectColors.darkTheme,
+                      boxShadow:
+                        themeState === false
+                          ? "0 0 5px 0 grey"
+                          : "0 0 5px 0 #ffffff",
+                      color: themeState === false ? "#000000" : "#ffffff",
+                      margin: "10px auto",
+                      borderRadius: "10px",
+                      padding: "2%",
+                    }}
+                  >
+                    <Grid container justifyContent={"space-between"}>
+                      <Grid item xs={7}>
+                        {atmBtn.map((data, index) => (
+                          <Button
+                          key={uuidv4()}
+                            sx={{
+                              color:
+                                themeState === false ? "#000000 " : "#ffffff",
+                            }}
+                          >
+                            <Box component={"span"}>
+                              {data?.num}
+                              <Box component={"sup"} sx={{ fontSize: "7px" }}>
+                                {data?.superScript}
+                              </Box>
+                            </Box>
+                          </Button>
+                        ))}
+                      </Grid>
+                      <Grid item xs={5}>
+                        <Button
+                          size="small"
+                          sx={{
+                            width: "100%",
+                            textAlign: "center",
+                            backgroundColor: projectColors.cancelBtnBgColor,
+                            color: "#000000",
+                            fontSize: "12px",
+                          }}
+                        >
+                          Cancel
+                        </Button>
+                        <Button
+                          size="small"
+                          sx={{
+                            width: "100%",
+                            textAlign: "center",
+                            backgroundColor: projectColors.clearBtnBgColor,
+                            color: "#000000",
+                            fontSize: "12px",
+                            mt: 1,
+                          }}
+                        >
+                          Clear
+                        </Button>
+                        <Button
+                        size="small"
+                          sx={{
+                            width: "100%",
+                            textAlign: "center",
+                            backgroundColor: projectColors.enterBtnBgColor,
+                            color: "#000000",
+                            fontSize: "12px",
+                            mt: 1,
+                          }}
+                        >
+                          Enter
+                        </Button>
+                      </Grid>
+                    </Grid>
                   </Box>
                 </Grid>
               </Grid>
